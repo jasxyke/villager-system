5bc69bac1e0c8a06de60a849fb0e10f106192b0f
VoidLaw-Cyber <rydelfabellon@gmail.com>
2024-08-15 15:44:10 +0800

    Home Page Content Update

--- mobile/app/(tabs)/home.jsx
+++ mobile/app/(tabs)/home.jsx
@@ -5,11 +5,14 @@ import AppHeader from "../../components/common/AppHeader";
 import { useAuthContext } from "../../context/AuthContext";
 import ScrollViewContainer from "../../components/forms/ScrollViewContainer";
 
+import image1 from '../../assets/images/maynilad.jpg';
+import image2 from '../../assets/images/hotline.png';
+
 const data = [
-  { id: '1', text: 'View 1' },
-  { id: '2', text: 'Voting of officers will be held at multi-purpose hall on June 20, 2023' },
-  { id: '3', text: 'Fur Parents Listen Up! Free vaccination for your pets will be held at a multi-purpose hall on June 30, 2023' },
-  { id: '4', text: 'View 4' },
+  { id: '1', text: 'Water Bill', image: image1 },
+  { id: '2', text: 'Voting of officers will be held at multi-purpose hall on June 20, 2023'},
+  { id: '3', text: 'Fur Parents Listen Up! Free vaccination for your pets will be held at a multi-purpose hall on June 30, 2023'},
+  { id: '4', text: 'Emergency Hotline', image: image2 },
 ];
 
 const Home = () => {
@@ -31,8 +34,12 @@ const Home = () => {
       <View style={styles.content}>
         <Text style={styles.greeting}>Hello, {user.firstname}!</Text>
         <View style={styles.row}>
-          <View style={styles.card}></View>
-          <View style={styles.card}></View>
+          <View style={styles.card}>
+            <Text style={styles.cardContent}>Bills</Text>
+          </View>
+          <View style={styles.card}>
+            <Text style={styles.cardContent}>Bookings</Text>
+          </View>
         </View>
         <ScrollViewContainer data={data} />
       </View>
@@ -47,31 +54,40 @@ const styles = StyleSheet.create({
   },
   centered: {
     flex: 1,
-
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   content: {
     width: '100%',
     alignItems: 'center',
-    padding: 20,
   },
   greeting: {
     color: 'white',
     fontSize: 24,
     width: '100%',
     marginBottom: 20,
+    marginLeft: 20,
   },
   row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '100%',
     marginBottom: 10,
+    padding: 20,
   },
   card: {
     borderRadius: 10,
     backgroundColor: '#1A2902',
     width: '45%',
     height: 200,
+    justifyContent: 'center',
+    padding: 10,
+    alignItems: 'center',
   },
+  cardContent: {
+    color: 'white',
+    fontSize: 18,
+  }
 });
 
 export default Home;

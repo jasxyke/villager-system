26488eb5e07a9766a86fcb12c19cccd67899c259
jasxyke01 <jaspher.cortez@growthops.asia>
2024-10-21 12:39:31 +0800

    edited the scrolling functionality of the sidebar

--- web-app/src/components/SideBar.jsx
+++ web-app/src/components/SideBar.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import homeIcon from "../assets/icons/Home_white.png";
 import bookingIcon from "../assets/icons/Calendar.png";
 import billsIcon from "../assets/icons/Book.png";
@@ -11,14 +11,16 @@ import styles from "./SideBar.module.css";
 import { useAuthContext } from "../contexts/AuthContext";
 import HouseIcon from "../assets/icons/temporaryhouse.png";
 import { CiMemoPad } from "react-icons/ci";
-import { CiReceipt } from "react-icons/ci";
-import { FiFileText, FiFlag } from "react-icons/fi";
-import { CiStickyNote } from "react-icons/ci";
 import { FaCar } from "react-icons/fa6";
+import { FiFileText, FiFlag } from "react-icons/fi";
 
 const SideBar = () => {
   const { logout } = useAuthContext();
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const activeLinkRef = useRef(null); // Reference to the active link
+
   const handleSucces = () => {
     //
   };
@@ -26,73 +28,99 @@ const SideBar = () => {
   const handleError = (msg) => {
     console.log(msg);
   };
+
   const logoutUser = () => {
     logout(handleSucces, handleError);
     navigate("/");
   };
+
+  // Scroll the sidebar to the active NavLink on mount or location change
+  useEffect(() => {
+    if (activeLinkRef.current) {
+      activeLinkRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+      });
+    }
+  }, [location.pathname]); // Trigger the effect when the route changes
+
   return (
     <nav className={styles.sidebar}>
       <ul>
-        {/* <li>
-          <NavLink to="/homepage">
-            <img src={homeIcon} alt="Home" /> Home
-          </NavLink>
-        </li> */}
         <li>
-          <NavLink to="/booking">
+          <NavLink
+            to="/booking"
+            ref={location.pathname === "/booking" ? activeLinkRef : null}
+          >
             <img src={bookingIcon} alt="Booking" /> Booking
           </NavLink>
         </li>
         <li>
-          <NavLink to="/bills">
-            <img src={billsIcon} alt="Bills" /> Collections
+          <NavLink
+            to="/bills"
+            ref={location.pathname === "/bills" ? activeLinkRef : null}
+          >
+            <img src={billsIcon} alt="Bills" /> Bills
           </NavLink>
         </li>
-        {/*<li>
-          <NavLink to="/files/house-permit">
-            <img src={filesIcon} alt="Files" /> Files
-          </NavLink>
-        </li>*/}
         <li>
-          <NavLink to="/permits">
+          <NavLink
+            to="/permits"
+            ref={location.pathname === "/permits" ? activeLinkRef : null}
+          >
             <CiMemoPad className="text-4xl mr-2" /> Permits
           </NavLink>
         </li>
         <li>
-          <NavLink to="/sticker">
+          <NavLink
+            to="/sticker"
+            ref={location.pathname === "/sticker" ? activeLinkRef : null}
+          >
             <FaCar className="text-3xl mr-2" /> Car Stickers
           </NavLink>
         </li>
         <li>
-          <NavLink to="/houses">
+          <NavLink
+            to="/houses"
+            ref={location.pathname === "/houses" ? activeLinkRef : null}
+          >
             <img src={HouseIcon} alt="Houses" /> Houses
           </NavLink>
         </li>
         <li>
-          <NavLink to="/users/residents">
+          <NavLink
+            to="/users/residents"
+            ref={
+              location.pathname === "/users/residents" ? activeLinkRef : null
+            }
+          >
             <img src={usersIcon} alt="Users" /> Users
           </NavLink>
         </li>
         <li>
-          <NavLink to="/announcements">
+          <NavLink
+            to="/announcements"
+            ref={location.pathname === "/announcements" ? activeLinkRef : null}
+          >
             <img src={announcementsIcon} alt="Announcements" /> Announcements
           </NavLink>
         </li>
         <li>
-          <NavLink to="/complaints">
+          <NavLink
+            to="/complaints"
+            ref={location.pathname === "/complaints" ? activeLinkRef : null}
+          >
             <FiFlag className="text-3xl mr-2" /> Complaints
           </NavLink>
         </li>
         <li>
-          <NavLink to="/reports">
+          <NavLink
+            to="/reports"
+            ref={location.pathname === "/reports" ? activeLinkRef : null}
+          >
             <FiFileText className="text-3xl mr-2" /> Reports
           </NavLink>
         </li>
-        {/* <li>
-          <NavLink to="/guidelines">
-            <CiReceipt className="text-4xl mr-2" /> Rules & Regulations
-          </NavLink>
-        </li> */}
         <li className="bg-transparent">
           <p className="" onClick={logoutUser}>
             <img src={logoutIcon} alt="Logout" /> Logout

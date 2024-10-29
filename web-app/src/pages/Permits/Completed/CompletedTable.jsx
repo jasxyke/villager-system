8031bed6c3e2373bc5c095b35be4ec88eb1f04a8
John Rey Rebusquillo <rebusquillo.johnrey.4102@gmail.com>
2024-10-29 18:53:20 +0800

    web app changes

--- web-app/src/pages/Permits/Completed/CompletedTable.jsx
+++ web-app/src/pages/Permits/Completed/CompletedTable.jsx
@@ -1,44 +1,66 @@
 import React, { useState } from "react";
 import PermitDetails from "../PermitDetails";
-import SamplePermits from "../SamplePermits";
+import usePermitRequests from "../../../hooks/usePermitRequests";
+import LoadingContainer from "../../../components/LoadingScreen/LoadingContainer";
 
-const CompletedTable = ({
-  onRowClick,
-  detailsView,
-  selectedPermit,
-  handleBack,
-}) => {
+const CompletedTable = () => {
+  const [detailsView, setDetailsView] = useState(false);
+  const [selectedPermit, setSelectedPermit] = useState(null);
+
+  const { permitRequests, loading, error } = usePermitRequests();
+
+  const handleRowClick = (permit) => {
+    setSelectedPermit(permit);
+    setDetailsView(true);
+  };
+
+  const handleBack = () => {
+    setSelectedPermit(null);
+    setDetailsView(false);
+  };
+
   return (
     <div className="overflow-x-auto">
       {detailsView ? (
         <PermitDetails permit={selectedPermit} onBack={handleBack} />
       ) : (
         <>
-          <div className="w-full">
-            <div className="flex items-center justify-center font-medium bg-mutedGreen mb-2 p-2 text-center">
-              <div className="flex-1 p-2 text-center">Name</div>
-              <div className="flex-1 p-2 text-center">Completed Date</div>
-              <div className="flex-1 p-2 text-center">Type</div>
-              <div className="flex-1 p-2 text-center">Status</div>
+          <div className="w-full mb-2">
+            <div className="flex items-center justify-center font-medium bg-mutedGreen p-2 text-center">
+              <div className="flex-1 p-2">Name</div>
+              <div className="flex-1 p-2">Completed Date</div>
+              <div className="flex-1 p-2">Type</div>
+              <div className="flex-1 p-2">Status</div>
             </div>
           </div>
+
           <div>
-            {SamplePermits.map((permit) => (
-              <div
-                key={permit.id}
-                className="flex border mb-2 hover:bg-darkerGreen cursor-pointer text-white"
-                onClick={() => onRowClick(permit)}
-              >
-                <div className="flex-1 p-2 text-center">{permit.name}</div>
-                <div className="flex-1 p-2 text-center">
-                  {permit.completionDate}
-                </div>
-                <div className="flex-1 p-2 text-center">
-                  {permit.permitType}
-                </div>
-                <div className="flex-1 p-2 text-center">{permit.status}</div>
+            {loading ? (
+              <LoadingContainer />
+            ) : error ? (
+              <p className="text-red-500">{error}</p>
+            ) : permitRequests.length === 0 ? (
+              <div className="text-center p-4 w-full">
+                No completed permits found.
               </div>
-            ))}
+            ) : (
+              permitRequests.map((permit) => (
+                <div
+                  key={permit.id}
+                  className="flex border mb-2 hover:bg-darkerGreen cursor-pointer text-white"
+                  onClick={() => handleRowClick(permit)}
+                >
+                  <div className="flex-1 p-2 text-center">
+                    {permit.resident.user.firstname}
+                  </div>
+                  <div className="flex-1 p-2 text-center">
+                    {permit.completed_date}
+                  </div>
+                  <div className="flex-1 p-2 text-center">{permit.purpose}</div>
+                  <div className="flex-1 p-2 text-center">{permit.status}</div>
+                </div>
+              ))
+            )}
           </div>
         </>
       )}
---
+++ web-app/src/pages/Permits/ToPay/ToPayPermit.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ToPayPermitTable from "./ToPayPermitTable";
+
+const ToPayPermit = () => {
+  return (
+    <div>
+      <div>
+        <ToPayPermitTable />
+      </div>
+    </div>
+  );
+};
+
+export default ToPayPermit;

9578dab995d6b50d9f553bec4195b8cffb07007b
jasxyke01 <jaspher.cortez@growthops.asia>
2024-12-09 13:21:14 +0800

    edited the sanctum config

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -15,12 +15,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),'192.168.254.122:5173',
-        '*'
-    ))),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf('*'))),
 
     /*
     |--------------------------------------------------------------------------

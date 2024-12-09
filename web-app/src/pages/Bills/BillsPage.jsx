a5fd88f1eca9760a424192d1d99e880d42c24684
jasxyke01 <jaspher.cortez@growthops.asia>
2024-12-09 23:37:11 +0800

    commented out the ovedue tab in the bills page

--- web-app/src/pages/Bills/BillsPage.jsx
+++ web-app/src/pages/Bills/BillsPage.jsx
@@ -33,24 +33,27 @@ const BillsPage = () => {
       <MainLogo />
       <div className="bg-gradient-to-t from-green to-mutedGreen shadow-lg rounded-lg overflow-hidden">
         <div className="flex">
-          {["overdue", "billList", "transactionHistory", "reminders"].map(
-            (filter) => (
-              <button
-                key={filter}
-                onClick={() => handleFilterClick(filter)}
-                className={`flex-1 text-center py-3 transition-all duration-300 ease-in-out ${
-                  activeFilter === filter
-                    ? "bg-paleDarkGreen text-white"
-                    : "bg-green  text-white hover:bg-paleDarkGreen"
-                } `}
-              >
-                {filter === "overdue" && "Overdue Bills"}
-                {filter === "billList" && "Bill List"}
-                {filter === "transactionHistory" && "Transaction History"}
-                {filter === "reminders" && "Reminders"}
-              </button>
-            )
-          )}
+          {[
+            // "overdue",
+            "billList",
+            "transactionHistory",
+            "reminders",
+          ].map((filter) => (
+            <button
+              key={filter}
+              onClick={() => handleFilterClick(filter)}
+              className={`flex-1 text-center py-3 transition-all duration-300 ease-in-out ${
+                activeFilter === filter
+                  ? "bg-paleDarkGreen text-white"
+                  : "bg-green  text-white hover:bg-paleDarkGreen"
+              } `}
+            >
+              {/* {filter === "overdue" && "Overdue Bills"} */}
+              {filter === "billList" && "Bill List"}
+              {filter === "transactionHistory" && "Transaction History"}
+              {filter === "reminders" && "Reminders"}
+            </button>
+          ))}
         </div>
 
         {/* Do not show Filters for "Overdue Bills" and "Reminders" */}
